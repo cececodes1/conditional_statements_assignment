daa5fe68f4b46b2e8dad3c7356be5ca27ab125e6
Sierra <sierradmichel@gmail.com>
2024-07-24 00:33:37 -0400

    Added decisions_at_crossroad.py

---
+++ decisions_at_crossroad.py
@@ -0,0 +1,10 @@
+# Get the user input
+number = float(input("Enter a number: "))
+
+# Corrected conditional statements
+if number > 0:
+    print("The number is positive.")
+elif number == 0:
+    print("The number is zero.")
+else:
+    print("The number is negative.")
